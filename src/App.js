8d05af482054ad05bd7b93ec5d933f72cd6af264
ROFLERGG <alexeysuper0@gmail.com>
2024-03-12 19:29:58 +0200

    added image

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './index.css'
 function Button({variant, children}) {
   if (variant === "primary") {
     return (
-      <button className='px-8 py-3 bg-sky-400 w-fit rounded-lg text-white font-roboto'>{children}</button>
+      <button className='px-8 py-3 bg-white border border-black w-fit rounded-lg text-black font-roboto shadow-btn'>{children}</button>
     );
-  } if (variant === "secondary") {
+  }
+  if (variant === "secondary") {
     return (
       <button className='px-8 py-3 bg-yellow-400 w-fit rounded-lg text-white font-roboto'>{children}</button>
     );
@@ -18,12 +19,36 @@ function Button({variant, children}) {
   };
 };
 
+function Header() {
+  return (
+    <header className='flex items-center px-8 py-4'>
+      <div className='flex-1'></div>
+      <div className='flex flex-1 justify-center space-x-6'>
+        <a href="" className='py-3 px-3 leading-none'>Find Jobs</a>
+        <a href="" className='py-3 px-3 leading-none'>For Companies</a>
+      </div>
+      <div className='flex flex-1 justify-end items-center space-x-6'>
+        <a href="" className='py-3 px-3 leading-none'>Sign In</a>
+        <Button variant={'primary'}>Sign up Free</Button>
+      </div>
+    </header>
+  )
+}
+
+function Hero() {
+  return (
+    <div className='flex flex-col min-h-screen'>
+      <Header/>
+      {/* content */}
+      <div className='bg-[url:]'></div>
+    </div>
+  )
+}
+
 function App() {
   return (
     // hero screen
-    <div className='bg-neutral-200 min-h-screen flex flex-col items-center justify-center'>
-      <Button variant={'primary'}>Sign up Free</Button>
-    </div>
+      <Hero/>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    container: {
+      screens: { 'default': '1300px' },
+      center: true,
+      padding: "16px"
+    },
     fontFamily: {
       roboto: ['"Roboto"', 'sans-serif'],
       work: ['"Work Sans"', 'sans-serif']
@@ -11,6 +16,10 @@ module.exports = {
     extend: {
       boxShadow: {
         'btn': '5px 5px 0px rgba(0,0,0,1)',
+      },
+      backgroundImage: {
+        'hero-pattern': "url('/')",
+        'footer-texture': "url('/img/footer-texture.png')",
       }
     },
   },
