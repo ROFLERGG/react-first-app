d8738b3f766078b8ce0e0a835f801ffd5d70977a
ROFLERGG <alexeysuper0@gmail.com>
2024-03-12 14:45:20 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './index.css'
 
-function Button() {
-  return (
-    <button className='px-6 py-1 bg-sky-400 w-fit rounded-lg text-white'>Log in</button>
-  );
+function Button({variant, children}) {
+  if (variant === "primary") {
+    return (
+      <button className='px-6 py-1 bg-sky-400 w-fit rounded-lg text-white'>{children}</button>
+    );
+  } else if (variant === "secondary") {
+    return (
+      <button className='px-6 py-1 bg-yellow-400 w-fit rounded-lg text-white'>{children}</button>
+    );
+  };
 };
 
 function App() {
   return (
     // hero screen
     <div className='bg-neutral-200 min-h-screen flex flex-col items-center justify-center'>
-      <Login/>
+      <Button variant={'secondary'}>Okay</Button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        work: ["Work Sans", 'sans-serif'],
+        roboto: ["Roboto", 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
